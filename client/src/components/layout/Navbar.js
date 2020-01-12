47c77ee9af5eafb1cd3da8da3b035bb48e9b9b75
Ayush <ayush.yushey@gmail.com>
2020-01-11 23:31:37 -0500

    live chat - commented out

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Widget, addResponseMessage  } from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css';
 
 class Navbar extends Component {
+  //live chat --------------------------
+  // handleNewUserMessage = (newMessage) => {
+  //   console.log(`New message incoming! ${newMessage}`);
+  //   // Now send the message throught the backend API
+  // }
+
+  // componentDidMount() {
+  //   addResponseMessage("Please send a message to start this chat.");
+  // }
+
   render() {
     return (
       <div className="navbar-fixed">
@@ -17,6 +30,13 @@ class Navbar extends Component {
               <i className="material-icons">code</i>
               JackPot
             </Link>
+            {/* <div className="App">
+              <Widget 
+                handleNewUserMessage={this.handleNewUserMessage}
+                title="Live Chat Support"
+                subtitle=""
+              />
+            </div> */}
           </div>
         </nav>
       </div>
