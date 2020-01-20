661eb0abb88e3191a6ec1b68cc3b4a84893f2ba1
Ayush <ayush.yushey@gmail.com>
2020-01-20 18:21:25 -0500

    proxy update test

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.use("/api/users", users);
 const port = process.env.PORT || 7000;
 
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(process.env.PORT, '0.0.0.0', () => console.log(`Server up and running on port ${port} !`));
